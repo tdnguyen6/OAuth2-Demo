3c4934c0e54912b6b3c5b50c4aba8a8c012d76d5
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2020-06-12 10:40:23 +0700

    fixing github oauth

--- ghOAuth2.js
+++ ghOAuth2.js
@@ -2,9 +2,13 @@ let ghSignIn = document.getElementById("gh-signIn");
 let ghSignOut = document.getElementById("gh-signOut");
 
 ghSignIn.onclick = () => {
-  fetch("https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize?client_id=d32d2e713ddf613284c2&state=hihihi", {
-    headers: {
-      "Origin": "*"
+  fetch(
+    "https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize?client_id=d32d2e713ddf613284c2&state=hihihi",
+    {
+      method: "GET",
+      headers: {
+        Origin: "*",
+      },
     }
-  });
+  );
 };
