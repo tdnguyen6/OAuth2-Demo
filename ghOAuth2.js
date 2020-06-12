c303500399ee05e82d7c28c4372b0e437d0805fc
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2020-06-12 10:30:18 +0700

    changed gg OAuth license

--- ghOAuth2.js
+++ ghOAuth2.js
@@ -2,5 +2,9 @@ let ghSignIn = document.getElementById("gh-signIn");
 let ghSignOut = document.getElementById("gh-signOut");
 
 ghSignIn.onclick = () => {
-  fetch("https://github.com/login/oauth/authorize?client_id=d32d2e713ddf613284c2&state=hihihi");
+  fetch("https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize?client_id=d32d2e713ddf613284c2&state=hihihi", {
+    headers: {
+      "origin": "*"
+    }
+  });
 };
