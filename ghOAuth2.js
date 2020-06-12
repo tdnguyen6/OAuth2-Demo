949b497af4b392b94df765d13c44d334f1bfd5e1
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2020-06-12 10:34:06 +0700

    added gg sign in

--- ghOAuth2.js
+++ ghOAuth2.js
@@ -4,7 +4,7 @@ let ghSignOut = document.getElementById("gh-signOut");
 ghSignIn.onclick = () => {
   fetch("https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/authorize?client_id=d32d2e713ddf613284c2&state=hihihi", {
     headers: {
-      "origin": "*"
+      "Origin": "*"
     }
   });
 };
