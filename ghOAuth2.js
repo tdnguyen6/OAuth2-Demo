d512db2af16b9794d5f57f4daf3b7e5f6d383195
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2020-06-12 14:10:53 +0700

    added github step 2

--- ghOAuth2.js
+++ ghOAuth2.js
@@ -15,11 +15,11 @@ if (queryString != "") {
 
   if (urlParams.get("code") != null && urlParams.get("state") != null) {
     let state = sessionStorage.getItem("state");
-    let code = sessionStorage.getItem("code");
+    let code = urlParams.get("code");
     if (urlParams.get("state") === state) {
       console.log(`Code is ${urlParams.get("code")}`);
       fetch(
-        "https://github.com/login/oauth/access_token?client_id=d32d2e713ddf613284c2&client_secret=a85d684c0fc52f12dbf56171a3d231eea610c671&code=" + code, { method: "POST" }
+        "https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token?client_id=d32d2e713ddf613284c2&client_secret=a85d684c0fc52f12dbf56171a3d231eea610c671&code=" + code, { method: "POST" }
       ).then(res => res.text()).then(ans => console.log(ans));
     }
   }
