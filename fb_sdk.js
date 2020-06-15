ac19f76ca4b73ef5ccaac10e091ad09cdfd4319a
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2020-06-16 01:15:30 +0700

    check fb api

--- fb_sdk.js
+++ fb_sdk.js
@@ -34,6 +34,7 @@ function checkLoginState() {
 function testAPI() {
   console.log("Welcome!  Fetching your information.... ");
   FB.api("/me", function (response) {
+    console.log(response);
     console.log("Successful login for: " + response.name);
     document.getElementById("status").innerHTML =
       "Thanks for logging in, " + response.name + "!";
