e934c084404f5f9e74e99c54fb78f7ec022f6899
TiDu Nguyen <tidu.nguyen.2000@gmail.com>
2020-06-16 10:49:33 +0700

    testing combined api call for fb

--- fb_sdk.js
+++ fb_sdk.js
@@ -33,18 +33,18 @@ function checkLoginState() {
 // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
 function testAPI() {
   console.log("Welcome!  Fetching your information.... ");
-  FB.api("/me", function (response) {
+  FB.api("/me?fields=id,name,picture", function (response) {
     console.log(response);
     console.log("Successful login for: " + response.name);
     document.getElementById("status").innerHTML =
       "Thanks for logging in, " + response.name + "!";
   });
-  FB.api("/me/picture", function (response) {
-    if (response && !response.error) {
-      /* handle the result */
-      console.log(response);
-    }
-  });
+  // FB.api("/me/picture?redirect=0", function (response) {
+  //   if (response && !response.error) {
+  //     /* handle the result */
+  //     console.log(response);
+  //   }
+  // });
 }
 
 function statusChangeCallback(response) {
